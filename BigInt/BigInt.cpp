de9a091ef38259742baf151e08055fc3bce2d2d5
Maksim Prudnikau <maksimprudnikau@gmail.com>
2021-01-20 09:58:45 +0300

    Инициализация + operator=

---
+++ BigInt/BigInt.cpp
@@ -0,0 +1,102 @@
+﻿#pragma once
+
+#include "BigInt.h"
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cassert>
+
+using namespace std;
+
+bool isNumb(string input)
+{
+	try
+	{
+		//Минус не влияет на принадлежность к int
+		if (input[0] == '-')
+		{
+			input.erase(0, 1);
+		}
+
+		//Проверка на наличие лишних нулей в начале числа
+		if (input[0] == '0' && input.size() != 1)
+		{
+			throw "Error - 01234 unsupported";
+		}
+
+		//Проверка на вхождение в диапазон [0,1]
+		for (char i : input)
+		{
+			if ((int)i < 48 || (int)i > 57)
+			{
+				throw "Error - illegal symbols (not in [0,1]) detected";
+			}
+		}
+	}
+	catch (const char* message)
+	{
+		cout << message << "\n";
+		return false;
+	}
+	return true;
+}
+
+BigInt::BigInt(const char* input) : neg(false)
+{
+	string buff(input);
+	assert(isNumb(buff) == true);
+
+
+	if (buff[0] == '-')
+	{
+		neg = true;
+		buff.erase(0, 1);
+	}
+
+	for each (char i in buff)
+	{
+		val.push_back(i - '0');
+	}
+}
+
+BigInt::BigInt(const string input) : BigInt(input.c_str()) {};
+
+BigInt& BigInt::operator= (const char* input)
+{
+	string buff(input);
+	assert(isNumb(buff) == true);
+
+	val.clear(); //Очистка предыдущего значения
+
+	if (buff[0] == '-')
+	{
+		neg = this->neg == true ? false : true;
+		buff.erase(0, 1);
+	}
+
+	for each (char i in buff)
+	{
+		val.push_back(i - '0');
+	}
+	return *this;
+}
+
+BigInt& BigInt::operator= (string input)
+{
+	assert(isNumb(input) == true);
+
+	val.clear(); //Очистка предыдущего значения
+
+	if (input[0] == '-')
+	{
+		neg = this->neg == true ? false : true;
+		input.erase(0, 1);
+	}
+
+	for each (char i in input)
+	{
+		val.push_back(i - '0');
+	}
+	return *this;
+}
---
+++ BigInt/BigInt.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class BigInt
+{
+private:
+	bool neg;
+	vector<int> val;
+public:
+
+	//Конструкторы BigInt x(to_string(...))
+	BigInt() = default;
+	BigInt(const char*);
+	BigInt(const string);
+
+	//Конструктор копирования запрещён
+	BigInt(const BigInt&) = delete;
+
+	//Операторы присваивания BigInt x = to_string(...)
+	BigInt& operator= (const char*);
+	BigInt& operator= (const string);
+
+	friend ostream& operator<< (ostream& out, const BigInt&);
+};
+
+ostream& operator<< (ostream& out, const BigInt& input)
+{
+	for each (int i in input.val)
+	{
+		out << i;
+	}
+	return out;
+}
